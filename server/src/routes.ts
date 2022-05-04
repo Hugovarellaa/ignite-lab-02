6a8d8f03f7196b02cc9926c235fd532c2fd67924
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-04 18:26:57 -0300

    Salvando no banco de dados, com os principios do SOLID

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import nodemailer from "nodemailer";
-import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -15,26 +16,26 @@ const transport = nodemailer.createTransport({
 
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
+  const prismaFeedback = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedback);
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
   });
 
-  await transport.sendMail({
-    from: "Equipe feedget <hugonoturno200@gmail.com>",
-    to: "Hugo <hugovarellaa@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-sereif; font-size: 16px; color: #111">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário : ${comment} </p>`,
-      `</div>`,
-    ].join("\n"),
-  });
+  // await transport.sendMail({
+  //   from: "Equipe feedget <hugonoturno200@gmail.com>",
+  //   to: "Hugo <hugovarellaa@gmail.com>",
+  //   subject: "Novo feedback",
+  //   html: [
+  //     `<div style="font-family: sans-sereif; font-size: 16px; color: #111">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário : ${comment} </p>`,
+  //     `</div>`,
+  //   ].join("\n"),
+  // });
 
-  return res.status(201).json({ data: feedback });
+  return res.status(201).send();
 });
