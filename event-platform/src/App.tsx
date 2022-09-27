8268eef683cd4cfcd5a67693a0b9a794412a2360
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-27 16:16:26 -0300

    Stage 2

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,37 +1,15 @@
-import { gql, useQuery } from "@apollo/client"
+
+import { Event } from "./pages/Event"
 import "./styles/main.css"
 
-interface Lesson {
-  id: string
-  title: string
-}
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
 
 export function App() {
-  const {data} = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
 
   return (
     <div className="">
-      <h1>Ola mundo</h1>
-      <ul>
-        {data?.lessons.map(lesson => (
-          <li key={lesson.id}>{lesson.title}</li>
-        ))}
-      </ul>
-
+    
+    <Event />
     </div>
   )
 }
