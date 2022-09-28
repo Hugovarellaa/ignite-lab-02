a23d57c51cbda73a309d391443a8273fef3e930e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-09-27 23:04:25 -0300

    Stage 4

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl8k5zcou1dpa01ul5w995inc/master",
+  uri: import.meta.env.VITE_API_URL, //variavel ambiente no ponto env
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN_GRAPHCSM}` //variavel ambiente no ponto env
+  },
   cache: new InMemoryCache()
 })
