dd8550333abe5253359f493ce379a443ceb1d2cc
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-01-21 19:51:58 -0300

    Delete event-platform directory

--- event-platform/src/lib/apollo.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL, //variavel ambiente no ponto env
-  headers: {
-    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN_GRAPHCSM}` //variavel ambiente no ponto env
-  },
-  cache: new InMemoryCache()
-})
